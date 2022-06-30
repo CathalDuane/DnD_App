8dc015d78fb733521d01093c1602d58db5bf382f
Cathal <duane.cathal@gmail.com>
2022-06-30 13:51:06 +0200

    added random

--- DiceApp.java
+++ DiceApp.java
@@ -1,8 +1,77 @@
-
+import java.util.Random;
 
 
 public class DiceApp {
-  
+    //vars for rolls
+    //Dice: 0 = 100, 1 = 20, 2 = 12, 3 = 10, 4 = 8, 5 = 6, 6 = 4
+    public int dice;
+    public int result;
+
+    //constructor
+    public DiceApp(){
+        result = 0;
+        dice = 0;
+    }
+
+    //setter
+    public void setRoll(int dice){
+        this.dice = dice;
+    }
+    //method
+    public int resultMethod(){
+       Random rand = new Random();
+
+
+       if(dice == 0){
+        int n = rand.nextInt(100);
+        result = n+1;
+        return result; 
+       }
+
+       else if(dice == 1){
+       int n = rand.nextInt(20);
+       result = n+1;
+       return result;
+       }
+
+       else if(dice == 2){
+        int n = rand.nextInt(12);
+        result = n+1;
+        return result;
+        }
+
+        else if(dice == 3){
+            int n = rand.nextInt(10);
+            result = n+1;
+          
+        }
+        else if(dice == 4){
+            int n = rand.nextInt(8);
+            result = n+1; 
+        }
+
+            else if(dice == 5){
+                int n = rand.nextInt(6);
+                result = n+1;
+                
+             }
+
+            else if(dice == 6){
+                int n = rand.nextInt(4);
+                result = n+1;
+              
+            }
+
+            return result;
+    }
+    
+
+    //getter
+    // public int getResult(){
+    //     return result;
+    // }
+
+
         
    
 }
--- DiceRollerApp.java
+++ DiceRollerApp.java
@@ -1,16 +1,28 @@
 import javax.swing.JOptionPane;
 
+
 public class DiceRollerApp {
     public static void main(String[] args) {
         int diceSelection;
+        int result;
+        int numOfDice;
 
         //Dice options
         String[] options = {"d100","d20","d12","d10","d8","d6","d4"};
 
+        // numOfDice = Integer.parseInt(JOptionPane.showInputDialog(null, "How many dice do you want to roll?"));
+
         //Button for selection dice
+       
         diceSelection = JOptionPane.showOptionDialog(null, "Select dice to roll", "Dice selector", JOptionPane.DEFAULT_OPTION, 0, null, options, options[6]);
-            
-        System.out.println(diceSelection);
+         
+     
+        DiceApp newRoll = new DiceApp();
+
+        newRoll.setRoll(diceSelection);
+        result = newRoll.resultMethod();
+
+        System.out.println(result);
         
         
 
